38b6192ca4e89b61b6e1b3766ae5badc134fd534
varadkarpe <varad.karpe>
2022-08-10 10:36:03 +0900

    UI Changes, REST API

--- base/views.py
+++ base/views.py
@@ -9,7 +9,7 @@ from unicodedata import name
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseBadRequest
 from .models import Message, Room, Topic
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from django.db.models import Q
 from django.contrib.auth.models import User
 
@@ -65,9 +65,10 @@ def home(request):
     q = request.GET.get('q') if request.GET.get('q') !=None else ''
     rooms = Room.objects.filter(
         Q(topic__name__icontains = q) | Q(name__icontains = q) | Q(description__icontains = q)) if q != '' else Room.objects.all()
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
-    context = {'rooms' : rooms, 'topics' : topics, 'room_count' : room_count}
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains = q))
+    context = {'rooms' : rooms, 'topics' : topics, 'room_count' : room_count, 'room_messages' : room_messages}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
@@ -80,18 +81,32 @@ def room(request, pk):
         )
         room.participants.add(request.user)
         return redirect('room', pk=room.id)
-    context = {'room' : room, 'usermessages' : room.message_set.all().order_by('-created'), 'participants' : room.participants.all()}
+    context = {'room' : room, 'user_messages' : room.message_set.all().order_by('-created'), 'participants' : room.participants.all()}
     return render(request, 'base/room.html', context)
+
+def userProfile(request, pk):
+    user = User.objects.get(id = pk)
+    rooms = user.room_set.all()
+    user_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user' : user, 'rooms' : rooms, 'room_messages' : user_messages, 'topics' : topics}
+    return render(request, 'base/profile.html', context)
 
 @login_required(login_url = '/login')
 def createRoom(request):
-    form = RoomForm({'host' : request.user})
+    form = RoomForm()
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    context = {'form' : form}
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name = topic_name)
+        #form = RoomForm(request.POST)
+        Room.objects.create(
+            host = request.user,
+            topic = topic,
+            name = request.POST.get('name'),
+            description = request.POST.get('description'),
+        )
+        return redirect('home')
+    context = {'form' : form, 'topics' : Topic.objects.all() }
     return render(request, 'base/room_form.html', context)
 
 @login_required(login_url= '/login')
@@ -101,11 +116,15 @@ def updateRoom(request, pk):
     if request.user != room.host:
         return HttpResponse('Request not valid')
     if request.method == 'POST':
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name = topic_name)
         form = RoomForm(request.POST, instance = room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    context = {'form' : form}
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
+    context = {'form' : form, 'topics' : Topic.objects.all(), 'room':room}
     return render(request, 'base/room_form.html', context)
 
 @login_required(login_url = '/login')
@@ -127,3 +146,14 @@ def deleteMessage(request, pk):
         message.delete()
         return redirect('room', pk = message.room.id)
     return render(request, 'base/delete.html', {'obj' : message})
+
+@login_required(login_url = 'login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance = user)
+    if request.method == 'POST' :
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+    return render(request, 'base/update-user.html', {'form' : form})
